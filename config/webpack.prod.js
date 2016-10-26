8ad203b49eb978ebc2a985294439036eb8d44305
Juan Manuel Verges <juanmanuel@verges.com>
2016-10-26 18:56:55 -0400

    Added webpack compression plugin

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -17,6 +17,7 @@ const NormalModuleReplacementPlugin = require('webpack/lib/NormalModuleReplaceme
 const ProvidePlugin = require('webpack/lib/ProvidePlugin');
 const UglifyJsPlugin = require('webpack/lib/optimize/UglifyJsPlugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 /**
  * Webpack Constants
@@ -191,10 +192,13 @@ module.exports = function (env) {
        * See: https://github.com/webpack/compression-webpack-plugin
        */
       //  install compression-webpack-plugin
-      // new CompressionPlugin({
-      //   regExp: /\.css$|\.html$|\.js$|\.map$/,
-      //   threshold: 2 * 1024
-      // })
+      new CompressionPlugin({
+          asset: '[path].gz[query]',
+          algorithm: 'gzip',
+          test: /\.css$|\.html$|\.js$|\.map$/,
+          threshold: 10240,
+          minRatio: 0.8
+      }),
 
       /**
        * Plugin LoaderOptionsPlugin (experimental)
